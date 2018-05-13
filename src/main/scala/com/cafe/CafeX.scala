020380402ffc8cc70946dae47857180bafa1497b
Akhilesh Kumar <akhilesh.kumar@digital.hmrc.gov.uk>
2018-05-13 12:16:08 +0100

    Intial Project setup with first failing test

---
+++ src/main/scala/com/cafe/CafeX.scala
@@ -0,0 +1,11 @@
+package com.cafe
+
+/**
+ * Created by akhileshkumar on 13/05/2018.
+ */
+class CafeX {
+
+  def generateBill()= {
+
+  }
+}
---
+++ src/test/scala/CafeXSpec.scala
@@ -0,0 +1,18 @@
+import com.cafe.CafeX
+import org.scalatest.{Matchers, WordSpec}
+
+/**
+ * Created by akhileshkumar on 13/05/2018.
+ */
+class CafeXSpec extends  WordSpec with Matchers {
+  val cafe = new CafeX
+  "CafeX" when {
+    "Calling generateBill" should {
+      "return number" in {
+        cafe.generateBill() shouldBe 1
+      }
+    }
+  }
+
+}
+
